933a06a7c780483fdcd312a36111412fc91c86cc
BillyS-PV <billsalme@gmail.com>
2023-05-22 21:00:37 -0500

    Subido

--- src/producto/main.ts
+++ src/producto/main.ts
@@ -1,23 +1,25 @@
-import { llenarData, productos } from "./producto.service";
+import { contador, llenarData, productos } from "./producto.service";
 
 llenarData({
   precio: 100,
-  name: "Billy",
-  stock: 6
+  name: "Audifonos",
+  stock: 1
 })
 console.log(productos[0]);
 
 llenarData({
   precio: 10,
   name: "Camara",
-  stock: 20
+  stock: 2
 });
 
 llenarData({
   precio: 200,
   name: "Celular",
-  stock: 100
+  stock: 10
 });
 
 console.log(productos[2]);
 console.log(productos);
+
+console.log(contador);
